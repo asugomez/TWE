196c1952b89c73583e9ea0d7545e818f306de0b3
Asuncion Gomez <asuncion.gomez@ug.uchile.cl>
2021-03-09 22:39:14 +0100

    tryit6 and others

--- TP2/js/script.js
+++ TP2/js/script.js
@@ -13,8 +13,20 @@ keypress
 var refImgLoad = null;
 var refBulle = null;
 
+var refButtonActu = null;
+var reFlecheID = null;
+
 var lastTO = false;
 var lastTP = false;
+
+var historique = [""]; //var cache  --> it's an array --> operations: push, pop, length
+var curseur = 0;
+var mode = "manuel";
+var iCache = 0; //indice tout ce que commence par "i", tout que commencer par "num" est un numero
+/*
+arr.length
+.push
+*/
 
 function init(){
     //console.log("init: fin chargement page"); //trace d'execution
@@ -23,6 +35,9 @@ function init(){
     refAutomatiquement = document.getElementById("automatiquement");
     refImgLoad = document.getElementById("imgLoad");
     refBulle = document.getElementById("infoBulle");
+
+    refButtonActu = document.getElementById("buttonActu");
+    refFlecheID = document.getElementById("flecheID");
     //lire l'état de la case (imposer un etat initia fixe) --> pour declancher la case (partie UX question 4eme version)
 }
 
@@ -37,10 +52,22 @@ function actualiser(){
 
 function actualiserPeriodiquement(){
     actualiser();
+    toggleImage();
     if(refAutomatiquement.checked){
+        //mode <-- automatique
+        mode = "auto";
+        refButtonActu.value = "Sauvegarder";
+        console.log("estoy aquiiiiiiiii");
+        console.log(refFlecheID);
+        refFlecheID.style.display = "inline";
         if(lastTP != false) clearInterval(lastTP);
         lastTP = window.setInterval(actualiser,3000);
+        
     } else{
+        //mode <-- manuel
+        mode = "manuel";
+        refButtonActu.value = "Actualiser";
+        refFlecheID.style.display = "none";
         if(lastTP!= false) clearInterval(lastTP);
         lastTP = false;
     }
@@ -53,13 +80,14 @@ function saisir(){
     if(refAutomatiquement.checked == true){
         //console.log("check true");
         actualiser();
+        //actualiserPeriodiquement();
     }
 
 }
 
 function toggleImage(){
     //inverser l'état de visibilité de l'image
-    if(refAutomatiquement.checked ==true){
+    if(refAutomatiquement.checked){
         refImgLoad.style.display = "inline";
     }
     else {
@@ -67,23 +95,35 @@ function toggleImage(){
     }
 }
 
-//si on appuie sur escape, la case disparaite 
+var observateur; 
 function annulerSaisieAuto(contexte) {
-	console.log("annulerSaisieAuto");
-	// comment savoir quelle est la touche qui a été appuyée ?
-	console.log(contexte);
+    // console.log("Appui sur " + contexte.which);
+    observateur = contexte; 
+    //key keyCode which code représentent la touche appuyée 
+    // ESC <=> 27 
+    if (contexte.which == 27) {
+        refAutomatiquement.checked = false;
+        actualiserPeriodiquement();
+        toggleImage();
+        // Quand on annule, on utilise les élements du visuel pour les réafficher dans le cadre 
+        refTxtSaisie.value = refCadreVisuel.innerHTML;
 
-	// https://caniuse.com permet de trouver quelle propriété est la plus standard
-	/*console.log("which" + contexte.which);
-	console.log("keyCode" + contexte.keyCode);
-	console.log("code" + contexte.code);*/
+        // https://caniuse.com permet de trouver quelle propriété est la plus standard
+        /*console.log("which" + contexte.which);
+        console.log("keyCode" + contexte.keyCode); 
+        console.log("code" + contexte.code);*/
 
-	if (contexte.code == "Escape") {
-		refAutomatiquement.checked = false;
-		toggleImage();
-	}
-
+        /*if (contexte.code == "Escape") {
+            refAutomatiquement.checked = false;
+            
+            actuPeriodique();
+            toggleImage();
+        }*/
+    }
 }
+
+//si on appuie sur escape, la case disparaite 
+
 
 function survoler(contexte){
     //console.log(contexte.target.id);
@@ -134,6 +174,25 @@ function cacherBulle(){
     refBulle.style.display = "none";
 }
 
+function reculer(){
+
+}
+
+function avancer(){
+
+}
+
+function ajouter(contenu){
+    historique.push(contenu);
+}
+
+function showHisto(){
+    console.log("pos: "+curseur + "(" + historique.length + " elts)");
+    var i;
+    for(i=0;i<historique.length;i++){
+        console.log("H["+i+"] : "+ historique[i]);
+    }
+}
 (function(global){
 
     function makeHTML(){
