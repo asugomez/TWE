42c8877d1a2ab9f3662b11736b1f97014e9eb97c
Asuncion Gomez <asuncion.gomez@ug.uchile.cl>
2021-03-18 07:17:08 +0100

    some files

--- TP2/CB1/js/script.js
+++ TP2/CB1/js/script.js
@@ -1,206 +1,92 @@
+/**
+ * variables
+ */
 
+var refChampEntree = null;
 
-//C'est une bonne practique de déclarer toutes les variables et les initialiser
-var refSaisie = null;
-var refVisuel = null;
-var refAutomatiquement = null;
-//propriete checked permet savoir l'information de la case à cocher 
-/*
-keyup -->
-keydown
-keypress
-*/
-var refImgLoad = null;
-var refBulle = null;
+var refButMenu = null;
+var refTitre1 = null;
+var refTitre2 = null;
+var refParagraphe = null;
+var refImage = null;
 
-var refButtonActu = null;
-var reFlecheID = null;
+var refMenuID =null;
 
-var lastTO = false;
-var lastTP = false;
-
-var historique = [""]; //var cache  --> it's an array --> operations: push, pop, length
-var curseur = 0;
-var mode = "manuel";
-var iCache = 0; //indice tout ce que commence par "i", tout que commencer par "num" est un numero
-/*
-arr.length
-.push
-*/
+var refEdition = null;
+/**
+ * functions
+ */
 
 function init(){
-    //console.log("init: fin chargement page"); //trace d'execution
-    refSaisie = document.getElementById("saisie");
-    refVisuel = document.getElementById("cadreVisuel");
-    refAutomatiquement = document.getElementById("automatiquement");
-    refImgLoad = document.getElementById("imgLoad");
-    refBulle = document.getElementById("infoBulle");
-
-    refButtonActu = document.getElementById("buttonActu");
-    refFlecheID = document.getElementById("flecheID");
-    //lire l'état de la case (imposer un etat initia fixe) --> pour declancher la case (partie UX question 4eme version)
-}
-
-function actualiser(){
-    console.log("actualiser");
-    //recuperer le contenu du textaerea
-    var contenu = refSaisie.value;
-    //console.log("contenu: "+ contenu);
-    //ecrire ce contenu dans le #cadreVisuel
-    refVisuel.innerHTML = contenu;
-}
-
-function actualiserPeriodiquement(){
-    actualiser();
-    toggleImage();
-    if(refAutomatiquement.checked){
-        //mode <-- automatique
-        mode = "auto";
-        refButtonActu.value = "Sauvegarder";
-        console.log("estoy aquiiiiiiiii");
-        console.log(refFlecheID);
-        refFlecheID.style.display = "inline";
-        if(lastTP != false) clearInterval(lastTP);
-        lastTP = window.setInterval(actualiser,3000);
-        
-    } else{
-        //mode <-- manuel
-        mode = "manuel";
-        refButtonActu.value = "Actualiser";
-        refFlecheID.style.display = "none";
-        if(lastTP!= false) clearInterval(lastTP);
-        lastTP = false;
-    }
-        
-}
+    refChampEntree = document.getElementById("champ-entree");
+    refButMenu = document.getElementById("button-menu");
+    //console.log(refButMenu.innerHTML);
+    console.log(refChampEntree);
+    refMenuID = document.getElementById("menuID");
 
 
-function saisir(){
-    //V4: on veut desactiver ce traitment 
-    if(refAutomatiquement.checked == true){
-        //console.log("check true");
-        actualiser();
-        //actualiserPeriodiquement();
-    }
-
-}
-
-function toggleImage(){
-    //inverser l'état de visibilité de l'image
-    if(refAutomatiquement.checked){
-        refImgLoad.style.display = "inline";
-    }
-    else {
-        refImgLoad.style.display= "none";
-    }
-}
-
-var observateur; 
-function annulerSaisieAuto(contexte) {
-    // console.log("Appui sur " + contexte.which);
-    observateur = contexte; 
-    //key keyCode which code représentent la touche appuyée 
-    // ESC <=> 27 
-    if (contexte.which == 27) {
-        refAutomatiquement.checked = false;
-        actualiserPeriodiquement();
-        toggleImage();
-        // Quand on annule, on utilise les élements du visuel pour les réafficher dans le cadre 
-        refTxtSaisie.value = refCadreVisuel.innerHTML;
-
-        // https://caniuse.com permet de trouver quelle propriété est la plus standard
-        /*console.log("which" + contexte.which);
-        console.log("keyCode" + contexte.keyCode); 
-        console.log("code" + contexte.code);*/
-
-        /*if (contexte.code == "Escape") {
-            refAutomatiquement.checked = false;
-            
-            actuPeriodique();
-            toggleImage();
-        }*/
-    }
-}
-
-//si on appuie sur escape, la case disparaite 
-
-
-function survoler(contexte){
-    //console.log(contexte.target.id);
-    //clientX, client Y: coordonnées souris sur l'écran 
-    //quand je survole la banniere (#nom)? quand le buttom?
-    //quel est l'element survole?
-    //contexte.target
-    var elementID= contexte.target.id;
+    refTitre1 = document.getElementById("titre1");
+    //console.log(refTitre1.innerHTML);
     
-    switch(elementID){
-        case "buttonActu": 
-            refBulle.innerHTML = "cliquer pour actualiser";
-            break;
-        case "automatiquement": 
-            refBulle.innerHTML = "cocher pour actualiser";
-            break;
-        case "labelAutour": 
-            refBulle.innerHTML = "cliquer pour cocher";
-            break;
-        default:
-            //refBulle.innerHTML= "";
-           return;    
-    }
-    //if(refBulle.innerHTML!=""){
-    refBulle.style.top=(parseInt(contexte.clientY) +15) + "px";
-    //console.log(contexte.clientY);
-
-    //window.innerWidth pour calculer et connaitre la tailler de l'écran
-    if(contexte.clientX < window.innerWidth/2) {
-        //bulle a droite
-        refBulle.style.left=(parseInt(contexte.clientX)) + "px";
-        refBulle.style.right="";
-    }
-    else{
-        //bulle a gauche
-        refBulle.style.left ="";
-        refBulle.style.right=(parseInt(window.innerWidth) - parseInt(contexte.clientX)) + "px";
-        
-    }
-    refBulle.style.display = "inline";
-
-    //cacher la boule dans 2 seconde
-    window.setTimeout(cacherBulle,1000)
-}
+    refTitre2 = document.getElementById("titre2");
+    refParagraphe = document.getElementById("paragraph");
+    refImage = document.getElementById("image");
 
 
-function cacherBulle(){
-    refBulle.style.display = "none";
-}
-
-function reculer(){
+    refEdition = document.getElementById("edition");
+    //console.log(refEdition.innerHTML);
 
 }
 
-function avancer(){
+function valider(){
+    content = refChampEntree.innerHTML;
+    /**
+     * if menu deroulant == titre1 --> 
+     * if titre2 -->
+     * paragrph -->
+     * imge-->
 
 }
 
-function ajouter(contenu){
-    historique.push(contenu);
+//fonction qui change le nom du bouton
+function changerNom(nom){
+    refButMenu.innerText = nom;
 }
 
-function showHisto(){
-    console.log("pos: "+curseur + "(" + historique.length + " elts)");
-    var i;
-    for(i=0;i<historique.length;i++){
-        console.log("H["+i+"] : "+ historique[i]);
-    }
+function changerNomT1(){
+    changerNom(refTitre1.innerHTML);
 }
-(function(global){
 
-    function makeHTML(){
+function changerNomT2(){
+    changerNom(refTitre2.innerHTML);
+}
 
-    }
+function changerNomPar(){
+    changerNom(refParagraphe.innerHTML);
+}
 
-    document.addEventListener("DOMContentLoaded", function (event) {
-    
-    });
+function changerIm(){
+    changerNom(refImage.innerHTML);
+}
 
-})(window);
+function emptyEdition(){
+    refEdition.innerHTML.value ="";
+}
+
+//semi-code from tryit_v6
+function appuiEntree(contexte){
+    // Number 13 is the "Enter" key on the keyboard
+    if (contexte.keyCode === 13)  {
+        valider();
+        emptyEdition();
+	}
+
+}
+
+//semi-code from tryit_v6
+function appuiEscape(contexte){
+    if (contexte.code == "Escape") {
+		emptyEdition();
+	}
+
+}
--- TP4/test_redir.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("maLibUtils.php");
-rediriger("vue_partie1.php",array("cle"=>"valeur", "cle2"=>"valeur2"));
-
-?>
